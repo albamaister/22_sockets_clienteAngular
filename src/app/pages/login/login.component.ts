f9b59d636d419e319e9c7c0bcea74d72a5ae5940
Andres Alba <bryanalba92@gmail.com>
2019-09-14 16:56:14 -0500

    Nombre de usuario y login template

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { WebsocketService } from '../../services/websocket.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  nombre = '';
+
+  constructor( public wsService: WebsocketService ) { }
 
   ngOnInit() {
+  }
+
+  ingresar() {
+    
+    this.wsService.loginWS( this.nombre );
   }
 
 }
